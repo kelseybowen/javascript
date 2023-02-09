d1909d483a32a2ec36b0f0e18b123bae613e22d8
Kelsey Bowen <kelseybowen22@gmail.com>
2023-02-09 14:46:55 -0800

    first commit

---
+++ first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Walk the dog</li>
+        <li>Climb Mount Everest</li>
+        <li>Make dinner</li>
+        <li>Take out the trash</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
