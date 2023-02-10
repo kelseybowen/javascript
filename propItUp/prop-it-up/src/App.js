ce58a4fc312601c82a0541c2c3afc32b38f9baee
Kelsey Bowen <kelseybowen22@gmail.com>
2023-02-10 15:39:07 -0800

    putting it together assignment

--- propItUp/prop-it-up/src/App.js
+++ propItUp/prop-it-up/src/App.js
@@ -4,10 +4,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Kelsey"} lastName={"Bowen"} age={30} hairColor={"Auburn"}/>
-      <PersonCard firstName={"David"} lastName={"Bowen"} age={31} hairColor={"Brown"}/>
-      <PersonCard firstName={"Maisy"} lastName={"Bowen"} age={6} hairColor={"Blonde"}/>
-      <PersonCard firstName={"Rhys"} lastName={"Bowen"} age={4} hairColor={"Blonde"}/>
+      <PersonCard firstName={"Kelsey"} lastName={"Bowen"} initialAge={30} hairColor={"Auburn"}/>
+      <PersonCard firstName={"David"} lastName={"Bowen"} initialAge={31} hairColor={"Brown"}/>
+      <PersonCard firstName={"Maisy"} lastName={"Bowen"} initialAge={6} hairColor={"Blonde"}/>
+      <PersonCard firstName={"Rhys"} lastName={"Bowen"} initialAge={4} hairColor={"Blonde"}/>
     </div>
   );
 }
--- propItUp/prop-it-up/src/components/PersonCard.jsx
+++ propItUp/prop-it-up/src/components/PersonCard.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
 
-    const {firstName, lastName, age, hairColor} = props;
+    const {firstName, lastName, initialAge, hairColor} = props;
+    const [ageState, setAgeState] = useState(initialAge);
     return (
         <div>
             <h1>{ lastName }, { firstName }</h1>
-            <p>Age: { age} </p>
+            <p>Age: { ageState } </p>
             <p>Hair Color: { hairColor }</p>
+            <button onClick={(event) => setAgeState(ageState + 1)}>Birthday Button for {firstName} {lastName}</button>
         </div>
     )
 }
