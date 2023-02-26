1edcf2fdecee882d630badaab07e8e587428fe2c
Kelsey Bowen <kelseybowen22@gmail.com>
2023-02-26 13:09:43 -0800

    added delete, complete functionality

--- toDoList/to-do-list/src/components/ToDo.jsx
+++ toDoList/to-do-list/src/components/ToDo.jsx
@@ -1,72 +1,78 @@
 import React, { useState } from 'react'
 
-const doneStyle = {
-    textDecoration: "line-through"
-};
+// const doneStyle = {
+//     textDecoration: "line-through"
+// };
 
 const ToDo = () => {
 
-    const [toDoItem, setToDoItem] = useState({
-        task: "",
-        completed: false
-    });
+    const [toDoItem, setToDoItem] = useState("");
 
     const [toDoList, setToDoList] = useState([]);
 
-    const handleChange = (e) => {
-        setToDoItem({ ...toDoItem, [e.target.name]: e.target.value });
-    }
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        setToDoList([...toDoList, toDoItem]);
-        setToDoItem({ task: "", completed: "" });
+        if (toDoItem.length === 0) {
+            return;
+        }
+        const newToDo = {
+            item: toDoItem,
+            complete: false
+        }
+
+        setToDoList([...toDoList, newToDo]);
+        setToDoItem("");
     }
 
-    const handleDelete = (e, item, i) => {
-        // console.log(item.completed);
+    const handleDone = (idx) => {
+        const updated = toDoList.map((item, i) => {
+            if (idx === i) {
+                item.complete = !item.complete;
+            }
+            return item;
+        });
+        setToDoList(updated);
+    }
 
-        setToDoList(item.completed, true);
+    const handleDelete = (deletedI) => {
+        const filtered = toDoList.filter((item, i) => {
+            return i !== deletedI;
+        })
+        setToDoList(filtered);
     }
 
     return (
         <div className="App container px-4">
-            <form className="form" onSubmit={handleSubmit}>
+            <form className="form" onSubmit={(e) => { handleSubmit(e) }}>
                 <h1 className='text-center py-4'>To Do List</h1>
                 <div className="my-3 container">
                     <label htmlFor="task">Add</label>
-                    <input 
-                        type="text" 
-                        className='form-control' 
-                        name="task" 
-                        value={toDoItem.task} 
-                        onChange={handleChange} />
-                    <input 
-                        type="hidden" 
-                        name="completed" 
-                        value={false} />
+                    <input
+                        type="text"
+                        className='form-control'
+                        name="task"
+                        value={toDoItem}
+                        onChange={(e) => { setToDoItem(e.target.value) }} />
                     <button className='btn btn-primary my-3'>Add</button>
                 </div>
             </form>
             <div className='my-3 container'>
-                <ul className="list-group">
-                    {
-                        toDoList.map((item, i) =>
-                            <li 
-                                style={item.completed ? doneStyle : null} 
-                                className='list-group-item' 
-                                name={item} 
-                                key={i}>{item.task}
-                                value={toDoList.item.task}
-                                <button 
-                                    className='btn btn-outline-danger float-end' 
-                                    key={i} 
-                                    value={toDoList.item.completed}
-                                    onClick={(e) => handleDelete(e, item, i)}>Delete</button>
-                            </li>
+                {
+                    toDoList.map((item, i) => {
+                        const classes = ["bold"];
+                        if (item.complete) {
+                            classes.push("line-through");
+                        }
+                        return (
+
+                            <div key={i} style={{ "display": "flex", "justifyContent": "space-evenly", "alignItems": "baseline" }}>
+                                <input type="checkbox" checked={item.complete} onChange={(e) => { handleDone(i) }} />
+                                <p className={classes.join(" ")}>{item.item}</p>
+                                <button className="btn btn-outline-danger" onClick={(e) => { handleDelete(i) }}>Delete</button>
+                            </div>
                         )
-                    }
-                </ul>
+                    })
+                }
             </div>
         </div>
     );
